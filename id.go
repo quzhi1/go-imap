78797bcd59fb10dc528d29aac7fde75824d4a11f
Zhi Qu <quzhi65222714@gmail.com>
2024-06-10 17:42:32 -0700

    wip

---
+++ id.go
@@ -0,0 +1,15 @@
+package imap
+
+type IdData struct {
+	Name        string
+	Version     string
+	Os          string
+	OsVersion   string
+	Vendor      string
+	SupportUrl  string
+	Address     string
+	Date        string
+	Command     string
+	Arguments   string
+	Environment string
+}
