0e4ef32841635e63d7562d8da2e549e973e213db
Zhi Qu <quzhi65222714@gmail.com>
2024-06-10 23:59:19 -0700

    Addressing comments

--- imapclient/id.go
+++ imapclient/id.go
@@ -7,25 +7,62 @@ import (
 	"github.com/emersion/go-imap/v2/internal/imapwire"
 )
 
-func (c *Client) Id(keysAndValues ...string) *IdCommand {
-	if len(keysAndValues)%2 != 0 {
-		panic("imapclient: the length of keys and values is odd")
-	}
-
+// Id sends an ID command.
+// ID command is introduced in RFC 2971.
+// An example ID command:
+// ID ("name" "go-imap" "version" "1.0" "os" "Linux" "os-version" "7.9.4" "vendor" "Yahoo")
+func (c *Client) Id(idData *imap.IdData) *IdCommand {
 	cmd := &IdCommand{}
 	enc := c.beginCommand("ID", cmd)
 
-	if len(keysAndValues) == 0 {
+	if idData == nil {
 		enc.SP().NIL()
 	} else {
 		enc.SP().Special('(')
-
-		for i, keyOrValue := range keysAndValues {
-			enc.Quoted(keyOrValue)
-
-			if i != len(keysAndValues)-1 {
-				enc.SP()
-			}
+		isFirstKey := true
+		if idData.Name != "" {
+			addIdKeyValue(enc, isFirstKey, "name", idData.Name)
+			isFirstKey = false
+		}
+		if idData.Version != "" {
+			addIdKeyValue(enc, isFirstKey, "version", idData.Version)
+			isFirstKey = false
+		}
+		if idData.Os != "" {
+			addIdKeyValue(enc, isFirstKey, "os", idData.Os)
+			isFirstKey = false
+		}
+		if idData.OsVersion != "" {
+			addIdKeyValue(enc, isFirstKey, "os-version", idData.OsVersion)
+			isFirstKey = false
+		}
+		if idData.Vendor != "" {
+			addIdKeyValue(enc, isFirstKey, "vendor", idData.Vendor)
+			isFirstKey = false
+		}
+		if idData.SupportUrl != "" {
+			addIdKeyValue(enc, isFirstKey, "support-url", idData.SupportUrl)
+			isFirstKey = false
+		}
+		if idData.Address != "" {
+			addIdKeyValue(enc, isFirstKey, "address", idData.Address)
+			isFirstKey = false
+		}
+		if idData.Date != "" {
+			addIdKeyValue(enc, isFirstKey, "date", idData.Date)
+			isFirstKey = false
+		}
+		if idData.Command != "" {
+			addIdKeyValue(enc, isFirstKey, "command", idData.Command)
+			isFirstKey = false
+		}
+		if idData.Arguments != "" {
+			addIdKeyValue(enc, isFirstKey, "arguments", idData.Arguments)
+			isFirstKey = false
+		}
+		if idData.Environment != "" {
+			addIdKeyValue(enc, isFirstKey, "environment", idData.Environment)
+			isFirstKey = false
 		}
 
 		enc.Special(')')
@@ -33,6 +70,14 @@ func (c *Client) Id(keysAndValues ...string) *IdCommand {
 	enc.end()
 
 	return cmd
+}
+
+func addIdKeyValue(enc *commandEncoder, isFirstKey bool, key, value string) {
+	if !isFirstKey {
+		enc.SP().Quoted(key).SP().Quoted(value)
+	} else {
+		enc.Quoted(key).SP().Quoted(value)
+	}
 }
 
 func (c *Client) handleId() error {
@@ -68,35 +113,37 @@ func (c *Client) readId(dec *imapwire.Decoder) (*imap.IdData, error) {
 
 		if currKey == "" {
 			currKey = keyOrValue
-		} else {
-			switch currKey {
-			case "name":
-				data.Name = keyOrValue
-			case "version":
-				data.Version = keyOrValue
-			case "os":
-				data.Os = keyOrValue
-			case "os-version":
-				data.OsVersion = keyOrValue
-			case "vendor":
-				data.Vendor = keyOrValue
-			case "support-url":
-				data.SupportUrl = keyOrValue
-			case "address":
-				data.Address = keyOrValue
-			case "date":
-				data.Date = keyOrValue
-			case "command":
-				data.Command = keyOrValue
-			case "arguments":
-				data.Arguments = keyOrValue
-			case "environment":
-				data.Environment = keyOrValue
-			default:
-				return fmt.Errorf("unknown key: %v", currKey)
-			}
-			currKey = ""
+			return nil
 		}
+		switch currKey {
+		case "name":
+			data.Name = keyOrValue
+		case "version":
+			data.Version = keyOrValue
+		case "os":
+			data.Os = keyOrValue
+		case "os-version":
+			data.OsVersion = keyOrValue
+		case "vendor":
+			data.Vendor = keyOrValue
+		case "support-url":
+			data.SupportUrl = keyOrValue
+		case "address":
+			data.Address = keyOrValue
+		case "date":
+			data.Date = keyOrValue
+		case "command":
+			data.Command = keyOrValue
+		case "arguments":
+			data.Arguments = keyOrValue
+		case "environment":
+			data.Environment = keyOrValue
+		default:
+			// Ignore unknown key
+			// Yahoo server sends "host" and "remote-host" keys
+			// which are not defined in RFC 2971
+		}
+		currKey = ""
 
 		return nil
 	})
@@ -112,6 +159,6 @@ type IdCommand struct {
 	data imap.IdData
 }
 
-func (r *IdCommand) Wait() imap.IdData {
-	return r.data
+func (r *IdCommand) Wait() (*imap.IdData, error) {
+	return &r.data, r.cmd.Wait()
 }
